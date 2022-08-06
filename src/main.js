3b91bb3d64915830d68099cc6318a265f98fdb31
master <neoway@126.com>
2022-08-06 21:59:33 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VModal from 'vue-js-modal'
+import vuetify from './plugins/vuetify'
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  VModal,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ]
 })
+
+module.exports ={
+  devServer: {
+    // 项目启动端口之后会变成3000
+    port: 81,
+     host:"0.0.0.0",
+  },
+
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
