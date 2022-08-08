35449eed3616fa03e54bc2c9d7f9837bb07a46c7
master <neoway@126.com>
2022-08-08 19:59:25 +0800

    init

--- src/components/helper/noFound.vue
+++ src/components/helper/noFound.vue
@@ -1,12 +1,15 @@
 <template>
   <div><h1 style="text-align: center">路径：{{$route.params.pathMatch}}未找到</h1></div>
+
 </template>
 
 <script>
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "noFound",
-  data: () => ({})
+  data: () => ({
+
+  })
 
 }
 </script>
