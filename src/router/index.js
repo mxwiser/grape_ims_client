8a9046bf2fe5155981ff5a25bd9bbb639b86bac7
master <neoway@126.com>
2022-08-18 23:32:02 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import VueRouter from "vue-router";
 import test from  '../components/test/entry'
-import testa from  '../components/test/entry'
 import notFound from "../components/helper/noFound"
+
+
 export default new VueRouter({
     mode:"history",
     routes:[
